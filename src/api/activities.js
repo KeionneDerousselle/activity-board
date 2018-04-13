6f778757a862f228354ff9d570d50dfaffaba55c
= <=>
2018-04-13 00:18:10 -0600

    Adding more test data

--- src/api/activities.js
+++ src/api/activities.js
@@ -2,7 +2,7 @@ export default [
   {
     'title': 'Snowboarding',
     'description': 'On the snow...with a board...with bros?',
-    'rating': 2,
+    'rating': 2.5,
     'type': 'Outdoor Activity',
     'tags': [],
     'price': 3.50
@@ -10,7 +10,7 @@ export default [
   {
     'title': 'Snowboarding',
     'description': 'On the snow...with a board...with bros?',
-    'rating': 2,
+    'rating': 3,
     'type': 'Outdoor Activity',
     'tags': [],
     'price': 3.50
@@ -18,7 +18,47 @@ export default [
   {
     'title': 'Snowboarding',
     'description': 'On the snow...with a board...with bros?',
-    'rating': 2,
+    'rating': 1.5,
+    'type': 'Outdoor Activity',
+    'tags': [],
+    'price': 3.50
+  },
+  {
+    'title': 'Snowboarding',
+    'description': 'On the snow...with a board...with bros?',
+    'rating': 1,
+    'type': 'Outdoor Activity',
+    'tags': [],
+    'price': 3.50
+  },
+  {
+    'title': 'Snowboarding',
+    'description': 'On the snow...with a board...with bros?',
+    'rating': 4,
+    'type': 'Outdoor Activity',
+    'tags': [],
+    'price': 3.50
+  },
+  {
+    'title': 'Snowboarding',
+    'description': 'On the snow...with a board...with bros?',
+    'rating': 4.5,
+    'type': 'Outdoor Activity',
+    'tags': [],
+    'price': 3.50
+  },
+  {
+    'title': 'Snowboarding',
+    'description': 'On the snow...with a board...with bros?',
+    'rating': 5,
+    'type': 'Outdoor Activity',
+    'tags': [],
+    'price': 3.50
+  },
+  {
+    'title': 'Snowboarding',
+    'description': 'On the snow...with a board...with bros?',
+    'rating': 3.5,
     'type': 'Outdoor Activity',
     'tags': [],
     'price': 3.50
