c68aa1a18f712e08012978c0e3bad68318b9b878
DarriusWrightGD <darriuswrightgd@gmail.com>
2018-04-13 22:38:28 -0600

    Filter bar updates

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,10 +1,17 @@
 import React, { PureComponent, Fragment } from 'react';
 // import { Form, FormGroup, FormText, Label, Input } from 'reactstrap';
-import { Rate, Form } from 'antd';
+import { Rate, Form, Checkbox } from 'antd';
 import styled, { css } from 'react-emotion';
-const FormItem = Form.Item;
+import PropTypes from 'prop-types';
 
-const FilterTitle = styled('h1') `
+const FormItem = Form.Item;
+const CheckboxGroup = Checkbox.Group
+
+const Container = styled('div')`
+  margin-left: .8rem;
+`
+
+const FilterByTitle = styled('h1') `
   color:white;
 `
 
@@ -14,6 +21,12 @@ const FormLabel = styled('span') `
   font-size:1.2rem;
 `
 
+const activities = [
+  'Apple', 
+  'Pear', 
+  'Orange'
+];
+
 class FilterBar extends PureComponent {
   render() {
     const { getFieldDecorator } = this.props.form;
@@ -22,11 +35,16 @@ class FilterBar extends PureComponent {
       wrapperCol: { span: 14 },
     };
     return (
-      <Fragment>
-        <FilterTitle>
+      <Container>
+        <FilterByTitle>
           Filter By
-        </FilterTitle>
+        </FilterByTitle>
         <Form>
+          <FormLabel>
+            Activity Types
+          </FormLabel>
+          <CheckboxGroup options={this.props.activityTypes}>
+          </CheckboxGroup>
           <FormLabel>
             Rating
           </FormLabel>
@@ -34,9 +52,13 @@ class FilterBar extends PureComponent {
             <Rate allowHalf defaultValue={1}/>
           </div>
         </Form>
-      </Fragment>
+      </Container>
     );
   }
+}
+
+FilterBar.propTypes = {
+  activityTypes: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 const FormFilterBar = Form.create()(FilterBar)
