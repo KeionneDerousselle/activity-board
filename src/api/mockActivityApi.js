a46b324b25d817e9171eb89509637bc9a64d1ab8
= <=>
2018-04-15 00:06:23 -0600

    Adding a mock api for activities.

---
+++ src/api/mockActivityApi.js
@@ -0,0 +1,19 @@
+import delay from './delay';
+import activities from './activities';
+
+
+// This file mocks a web API by working with the hard-coded data in activities.js.
+// It uses setTimeout to simulate the delay of an AJAX call.
+// All calls return promises.
+
+class ActivityApi {
+  static getAllActivities = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({...activities});
+      }, delay);
+    });
+  }
+}
+
+export default ActivityApi;
