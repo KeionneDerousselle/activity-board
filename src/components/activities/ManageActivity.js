065c58fe3be414230c0c1d477ad0fdf7cf9e81d2
= <=>
2018-04-15 18:00:42 -0600

    Connecting the edit activity container component to the store

--- src/components/activities/ManageActivity.js
+++ src/components/activities/ManageActivity.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-const ManageActivity = () => 
-  <div>
-    Managing Activity
-  </div>;
+class ManageActivity extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      activity: { ...props.activity }
+    };
+  }
 
-export default ManageActivity;
+  componentWillReceiveProps(nextProps) {
+    if (this.props.activity.id !== nextProps.activity.id) {
+      this.setState({ activity: { ...nextProps.activity } });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.activity.id}
+      </div>
+    );
+  }
+}
+
+ManageActivity.propTypes = {
+  activity: PropTypes.object.isRequired
+};
+
+ManageActivity.contextTypes = {
+  router: PropTypes.object
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const activityId = ownProps.match.params.id;
+  let activity = {
+    id: 'you will create an activity',
+
+  };
+
+  if (activityId && state.activities.items.length > 0) {
+    activity = {
+      id: activityId
+    };
+  }
+
+  return {
+    activity: activity
+  };
+};
+
+export default connect(mapStateToProps)(ManageActivity);
