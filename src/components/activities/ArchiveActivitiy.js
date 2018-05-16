c2657c76511de3937fd94d4fe85bbaccb80933bd
= <=>
2018-05-15 23:13:10 -0600

    Handling submit archive date for activity

--- src/components/activities/ArchiveActivitiy.js
+++ src/components/activities/ArchiveActivitiy.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { saveActivity } from './actions/activity.actions';
 import ArchiveForm from './ArchiveForm';
-import { Modal } from 'antd';
+import { Modal, notification } from 'antd';
 const confirm = Modal.confirm;
 
 
@@ -10,21 +13,55 @@ class ArchiveActivity extends React.Component {
     saving: false
   }
 
-  handleOnSubmit = (archivalDate, archivedUntilText, displayDate) => {
+  handleOnSubmit = (archiveDate, archivedUntilText, displayDate) => {
     const { activity } = this.props;
+
     confirm({
       title: 'Are you sure?',
       content: `You are about to archive ${activity.title} ${archivedUntilText}. This means ${activity.title} will not be shown on the activities dashboard or be suggested until ${displayDate}.`,
       okText: 'Yes',
       cancelText: 'No',
-      onOk() {
-        alert('OK');
-      },
+      onOk: () => this.archiveActivity(archiveDate),
       onCancel() {
+        // TODO: handle clear form
         alert('Cancel');
       },
     });
   }
+  redirect = () => {
+    const { activity, history } = this.props;
+    history.push(`/activity/${activity.id}`);
+  }
+
+  archiveActivity = archiveDate => {
+    const { activity, saveActivity } = this.props;
+    activity.archivedUntil = archiveDate;
+    this.setState({ saving: true });
+
+    saveActivity(activity)
+      .then(() => this.handleSaveArchiveSuccess())
+      .catch(() => this.handleSaveArchiveFailed());
+  };
+
+  handleSaveArchiveSuccess = () => {
+    const { activity: { title } } = this.props;
+    this.setState({ saving: false });
+    this.redirect();
+    notification.success({
+      message: 'Success!',
+      description: `${title} was successfully archived.`,
+      duration: 4
+    });
+  };
+
+  handleSaveArchiveFailed = () => {
+    this.setState({ saving: false });
+    notification.error({
+      message: 'Error',
+      description: 'This activity could not be archived. Please validate all fields and try again.',
+      duration: 4
+    });
+  };
 
   render() {
     return (
@@ -37,7 +74,13 @@ class ArchiveActivity extends React.Component {
 }
 
 ArchiveActivity.propTypes = {
-  activity: PropTypes.object.isRequired
+  activity: PropTypes.object.isRequired,
+  saveActivity: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired
 };
 
-export default ArchiveActivity;
+const mapDispatchToProps = {
+  saveActivity
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(ArchiveActivity));
