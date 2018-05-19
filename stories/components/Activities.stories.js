83732d5ac0c682dfd919b296c8f5b966c626531e
= <=>
2018-05-18 23:53:28 -0600

    removing storybook

--- stories/components/Activities.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Activities } from '../../src/components';
-import { storiesOf } from '@storybook/react';
-import activities from '../../src/api/activities';
-
-storiesOf('Activities', module)
-  .add('basic', () => <Activities activities={activities}/>);
